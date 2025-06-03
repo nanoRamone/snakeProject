8ae680d7899f0f13285ac33db9b7e93628184a19
Facundo Garay <facundogaray.ac@gmail.com>
2025-06-03 16:42:44 -0300

    Facu 0.2

---
+++ src/main/java/com/mycompany/juego/Mision.java
@@ -0,0 +1,35 @@
+package com.mycompany.juego;
+
+import java.util.List;
+
+public abstract class Mision {
+    protected Mapa mapa;
+    protected Snake snake;
+    protected List<Guardia> guardias;
+
+    /**  
+     * Monta el escenario: crea mapa, coloca Snake, guardias, items, puertas…
+     */
+    public abstract void configurar();
+
+    /**  
+     * Devuelve el mapa ya configurado.  
+     */
+    public Mapa getMapa() {
+        return mapa;
+    }
+
+    /**  
+     * Devuelve la instancia de Snake en este escenario.  
+     */
+    public Snake getSnake() {
+        return snake;
+    }
+
+    /**  
+     * Devuelve la lista de guardias en este escenario.  
+     */
+    public List<Guardia> getGuardias() {
+        return guardias;
+    }
+}
---
+++ src/main/java/com/mycompany/juego/MisionInicial.java
@@ -0,0 +1,44 @@
+package com.mycompany.juego;
+
+import java.util.ArrayList;
+
+public class MisionInicial extends Mision {
+
+    @Override
+    public void configurar() {
+        // 1) Mapa 7×7
+        mapa = new Mapa(7, 7);
+
+        // 2) Snake en posición aleatoria libre
+        Posicion p;
+        do { p = mapa.generarPosicionAleatoria(); }
+        while (!mapa.getCelda(p.getX(), p.getY()).estaVacia());
+        snake = new Snake("Snake", p);
+        mapa.getCelda(p.getX(), p.getY()).setContenido(snake);
+
+        // 3) Puerta fija y llave
+        Puerta puerta = new Puerta("Puerta del Hangar");
+        mapa.getCelda(0, 3).setContenido(puerta);
+
+        Posicion pl;
+        do { pl = mapa.generarPosicionAleatoria(); }
+        while (!mapa.getCelda(pl.getX(), pl.getY()).estaVacia());
+        Item llave = new Item("Llave", puerta);
+        mapa.getCelda(pl.getX(), pl.getY()).setContenido(llave);
+
+        // 4) Guardias a ≥2 de distancia
+        guardias = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Posicion pg;
+            do {
+                pg = mapa.generarPosicionAleatoria();
+            } while (
+                !mapa.getCelda(pg.getX(), pg.getY()).estaVacia() ||
+                pg.distancia(p) < 2
+            );
+            Guardia g = new Guardia("Guardia" + (i+1), pg);
+            mapa.getCelda(pg.getX(), pg.getY()).setContenido(g);
+            guardias.add(g);
+        }
+    }
+}
